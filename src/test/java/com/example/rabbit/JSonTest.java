f1201f69468652f450960509fb413cf721b4b9c7
ninuxGithub <1732768613@qq.com>
2017-09-15 15:45:03 +0800

    java 采用tcp方式调用golang

---
+++ src/test/java/com/example/rabbit/JSonTest.java
@@ -0,0 +1,40 @@
+package com.example.rabbit;
+
+import java.net.*;
+import java.io.*;
+import net.sf.json.JSONObject;
+
+public class JSonTest {
+
+	// 查看端口被占用的tcp 的 PID
+	// netstat -ano | findstr 50000
+	// 杀死tcp
+	// taskkill /pid 11360 /f
+	private static Socket socket;
+	private static BufferedReader in;
+	private static PrintWriter out;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JSONObject jsonObj = new JSONObject();
+		jsonObj.put("Name", "liangyongs");
+		jsonObj.put("Id", 31);
+		String[] likes = { "java", "golang", "clang" };
+		jsonObj.put("Lks", likes);
+		System.out.println("Object before sending to golang side:");
+		System.out.println(jsonObj);
+		try {
+			socket = new Socket("127.0.0.1", 50000);
+			in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			out = new PrintWriter(socket.getOutputStream(), true);
+			out.println(jsonObj);
+			String line = in.readLine();
+			System.out.println("Object read from golang side:");
+			jsonObj = JSONObject.fromObject(line);
+			System.out.println(jsonObj.get("Id"));
+			socket.close();
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+	}
+}
